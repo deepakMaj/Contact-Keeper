9d59b320d825e6151e83ca570edfdfa478573bbb
Deepak Mahajan <deepakmahajan@gmail.com>
2020-09-12 20:25:12 +0530

    Contact-Keeper

--- server.js
+++ server.js
@@ -2,14 +2,22 @@ const express = require('express');
 const connectDB = require('./config/db');
 const app = express();
 const cors = require('cors');
+const path = require('path');
 
 connectDB();
 app.use(express.json({ extended: false })); // Initiate middleware
 app.use(cors());
-app.get('/', (req, res) => res.json({ msg: 'Welcome to the ContactKeeper API...' }));
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
